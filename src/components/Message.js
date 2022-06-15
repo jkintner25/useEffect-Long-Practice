8aa905ed9827d17968e5b4616d567cf5a32316f9
David Rivera <daleriro@gmail.com>
2022-06-15 19:23:43 -0400

    Finished Last Phase

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,17 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
 
-function Message({ size }) {
+function Message({ size, featherCount }) {
+  const [sizeSelection, setSizeSelection] = useState();
 
-    useEffect(()=>{
-        console.log('Message', size);
-    }, [size])
+  useEffect(() => {
+    console.log('PictureDisplay size', size);
+    let cname = '';
+    switch (size) {
+      case 's':
+        cname = 'small';
+        break;
+      case 'm':
+        cname = 'medium';
+        break;
+      case 'l':
+        cname = 'large';
+        break;
+      case 'xl':
+        cname = 'xlarge';
+        break;
+      default:
+        cname = 'small';
+        break;
+    }
+    setSizeSelection(cname);
+  }, [size]);
 
-
-    return (
-        <div className="message medium">
-            (Oh my! Your bird is naked!)
-        </div>
-    );
-};
+  return (
+    <div className={`message ${sizeSelection}`}>
+      {!featherCount
+        ? 'Oh my your bird is naked!'
+        : `Your bird has ${featherCount} feathers!`}
+    </div>
+  );
+}
 
 export default Message;
